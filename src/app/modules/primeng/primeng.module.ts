b2ebb1ebb0160bf5e3a0b169a22863ef03017fb5
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2021-10-17 11:40:50 -0300

    shared and primeng modules

---
+++ src/app/modules/primeng/primeng.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PanelModule } from 'primeng/panel';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, PanelModule],
+  exports: [PanelModule],
+})
+export class PrimengModule {}

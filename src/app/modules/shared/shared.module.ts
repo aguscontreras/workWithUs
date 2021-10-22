56e855c63257af7a214c00c86573841913f94bcb
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2021-10-21 23:33:40 -0300

    modulos

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { LoginComponent } from './pages/login/login.component';
 import { PrimengModule } from '../primeng/primeng.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [NotFoundComponent, LoginComponent],
-  imports: [CommonModule, PrimengModule],
+  imports: [CommonModule, PrimengModule, ReactiveFormsModule],
 })
 export class SharedModule {}

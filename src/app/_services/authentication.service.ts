5f9caaaf014491a958bc325598387af6275a1389
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2021-10-21 23:32:39 -0300

    auth service

---
+++ src/app/_services/authentication.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../_models';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticationService {
+  private userSubject: BehaviorSubject<User>;
+  public user$: Observable<User>;
+
+  constructor(private router: Router, private http: HttpClient) {
+    this.userSubject = new BehaviorSubject<User>(
+      JSON.parse(localStorage.getItem('user'))
+    );
+
+    this.user$ = this.userSubject.asObservable();
+  }
+
+  public get userValue(): User {
+    return this.userSubject.value;
+  }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http
+      .post<any>(`${environment.apiUrl}/users/authenticate`, {
+        username,
+        password,
+      })
+      .pipe(
+        map((user) => {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.userSubject.next(user);
+          return user;
+        })
+      );
+  }
+
+  logout(): void {
+    localStorage.removeItem('user');
+    this.userSubject.next(null);
+    this.router.navigate(['/login']);
+  }
+}

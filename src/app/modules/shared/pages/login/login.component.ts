5b6ca1f8b5e281d7c4fa14b29b99de418b18af95
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2021-10-21 23:33:04 -0300

    creando login

--- src/app/modules/shared/pages/login/login.component.ts
+++ src/app/modules/shared/pages/login/login.component.ts
@@ -1,16 +1,76 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { AuthenticationService } from '../../../../_services';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  error = '';
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private authenticationService: AuthenticationService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    if (this.authenticationService.userValue) {
+      this.router.navigate(['/']);
+    }
 
-  ngOnInit(): void {
+    this.createLoginForm();
   }
 
+  ngOnInit(): void {}
+
+  private createLoginForm(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  get f(): {
+    [key: string]: AbstractControl;
+  } {
+    return this.loginForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+
+    this.authenticationService
+      .login(this.f.username.value, this.f.password.value)
+      .pipe(first())
+      .subscribe({
+        next: () => {
+          const returnUrl =
+            this.route.snapshot.queryParams.get('returnUr') || '';
+          this.router.navigateByUrl(returnUrl);
+        },
+
+        error: (err) => {
+          this.error = err;
+          this.loading = false;
+        },
+      });
+  }
 }

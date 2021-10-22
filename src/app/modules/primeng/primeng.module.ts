999cb9602c15cc76279d12f99514a579f13203ee
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2021-10-21 23:32:49 -0300

    modulos

--- src/app/modules/primeng/primeng.module.ts
+++ src/app/modules/primeng/primeng.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PanelModule } from 'primeng/panel';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, PanelModule],
-  exports: [PanelModule],
+  imports: [CommonModule, PanelModule, InputTextModule, ButtonModule],
+  exports: [PanelModule, InputTextModule, ButtonModule],
 })
 export class PrimengModule {}

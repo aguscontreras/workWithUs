898a4b9c5ec328ce58e7d42b895d61f17c799ece
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2021-10-23 18:55:55 -0300

    rutas home y register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './modules/shared/pages/login/login.component';
 import { NotFoundComponent } from './modules/shared/pages/not-found/not-found.component';
+import { HomeComponent } from './modules/shared/pages/home/home.component';
+import { RegisterComponent } from './modules/common/pages/register/register.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'register', component: RegisterComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: '**', component: NotFoundComponent },
 ];
 

ffe9b53022f9ebb3a117ff7f6b7e7c265324136f
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2021-10-23 18:56:23 -0300

    creando home

---
+++ src/app/modules/shared/pages/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+  email: string;
+  constructor(private router: Router) {}
+
+  onClickBtnRegistrar(): void {
+    this.router.navigate(['/register'], {
+      queryParams: { email: this.email },
+    });
+  }
+}

90e9704ab25ce4a0bd710e76b36544ff87f98b84
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2021-10-23 18:56:35 -0300

    imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimengModule } from './modules/primeng/primeng.module';
 import { SharedModule } from './modules/shared/shared.module';
 import { fakeBackendProvider } from './_helpers';
+import { RegisterComponent } from './modules/common/pages/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RegisterComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
+    FormsModule,
     ReactiveFormsModule,
     PrimengModule,
     SharedModule,
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -4,9 +4,11 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { LoginComponent } from './pages/login/login.component';
 import { PrimengModule } from '../primeng/primeng.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [NotFoundComponent, LoginComponent],
-  imports: [CommonModule, PrimengModule, ReactiveFormsModule],
+  declarations: [NotFoundComponent, LoginComponent, HomeComponent],
+  imports: [CommonModule, PrimengModule, ReactiveFormsModule, FormsModule],
 })
 export class SharedModule {}

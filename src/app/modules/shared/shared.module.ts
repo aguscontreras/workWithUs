ecf59a3c68433c9e64812e09e1da2e7eeb895dda
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2021-10-27 10:40:45 -0300

    .

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -6,16 +6,27 @@ import { PrimengModule } from '../primeng/primeng.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { ForgotComponent } from './pages/forgot/forgot.component';
 
 @NgModule({
   declarations: [
     NotFoundComponent,
     LoginComponent,
+    RegisterComponent,
     HomeComponent,
     NavbarComponent,
+    ForgotComponent,
   ],
-  imports: [CommonModule, PrimengModule, ReactiveFormsModule, FormsModule],
+  imports: [
+    CommonModule,
+    PrimengModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule,
+  ],
   exports: [NavbarComponent],
 })
 export class SharedModule {}

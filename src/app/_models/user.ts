c669ee4732a9f5a4f762b50b25762e7b0837a97f
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2021-10-27 09:05:05 -0300

    constructor clase user

--- src/app/_models/user.ts
+++ src/app/_models/user.ts
@@ -2,9 +2,26 @@ import { Role } from './role';
 
 export class User {
   id: number;
-  firstName: string;
-  lastName: string;
+  firstName = '';
+  lastName = '';
   username: string;
   role: Role;
   token?: string;
+  password?: string;
+
+  constructor(username: string, password: string, role: Role) {
+    this.username = username;
+    this.password = password;
+    this.role = role;
+    this.id = this.getId();
+  }
+
+  private getId(): number {
+    const currentUsers: User[] =
+      JSON.parse(localStorage.getItem('users')) || [];
+    const lastId = currentUsers.length
+      ? currentUsers[currentUsers.length - 1].id
+      : 0;
+    return lastId + 1;
+  }
 }

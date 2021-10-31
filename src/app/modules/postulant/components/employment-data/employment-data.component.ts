3d7e994700b30f329b006bf0826238c8a474d7eb
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2021-10-30 22:43:30 -0300

    pestaña datos experiencia

---
+++ src/app/modules/postulant/components/employment-data/employment-data.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employment-data',
+  templateUrl: './employment-data.component.html',
+  styleUrls: ['./employment-data.component.scss'],
+})
+export class EmploymentDataComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
